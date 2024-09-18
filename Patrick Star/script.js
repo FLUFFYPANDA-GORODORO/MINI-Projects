42896e042d90d22bb5d858b4e5d96dad284705ae
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2024-09-18 18:38:52 +0530

    Create script.js

---
+++ Patrick Star/script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+const mobilenav = document.querySelector(".mobilenav");
+
+hamburger.addEventListener("click",() => {
+    bar1.classList.toggle("animateBar1")
+    bar2.classList.toggle("animateBar2")
+    bar3.classList.toggle("animateBar3")
+    mobilenav.classList.toggle("opendrawer")
+})
