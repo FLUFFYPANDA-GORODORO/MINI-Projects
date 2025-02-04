dd3953bc5921cf7ca2d96b30f8c78eb3dc4175f0
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2025-02-04 15:36:10 +0530

    Update Marque.jsx

--- Refokus/src/components/Marque.jsx
+++ Refokus/src/components/Marque.jsx
@@ -4,13 +4,13 @@ import { motion } from "motion/react";
 function Marque({ imagesurls , direction ,direction1 }) {
   return (
     <div className="flex w-full overflow-hidden mt-12 h-20">
-      <motion.div intial={{x: direction === 'left' ? "0" : "-100%"}} animate={{x: direction === 'left' ? "-100%" : "0"}} transition={{ease:"linear",duration:10 ,repeat:Infinity}} className="flex flex-shrink-0  pr-10 gap-20">
+      <motion.div initial={{x: direction === 'left' ? "0" : "-100%"}} animate={{x: direction === 'left' ? "-100%" : "0"}} transition={{ease:"linear",duration:10 ,repeat:Infinity}} className="flex flex-shrink-0  pr-10 gap-20">
         {imagesurls.map((url) => (
           <img src={url} />
         ))}
       </motion.div>
      
-      <motion.div intial={{x: direction === 'left' ? "0" : "-100%"}} animate={{x: direction === 'left' ? "-100%" : "0"}} transition={{ease:"linear",duration:10 ,repeat:Infinity}} className="flex flex-shrink-0  pr-10 gap-20">
+      <motion.div initial={{x: direction === 'left' ? "0" : "-100%"}} animate={{x: direction === 'left' ? "-100%" : "0"}} transition={{ease:"linear",duration:10 ,repeat:Infinity}} className="flex flex-shrink-0  pr-10 gap-20">
         {imagesurls.map((url) => (
           <img src={url} />
         ))}
